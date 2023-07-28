dad46938e363cd9bed705fc7420d94d6c42cfd79
Surya Singh <surya.scoe3154@gmail.com>
2023-07-28 09:56:46 +0530

    Getting all time watch movie recommendation by doing web scraping using Beautiful Soup.

--- Starting Code - 100 movies to watch start/main.py
+++ Starting Code - 100 movies to watch start/main.py
@@ -4,5 +4,17 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(url=URL)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, "html.parser")
+raw_movies = soup.find_all(name="h3", class_="title")
+movies = [movie.getText() for movie in raw_movies]
+# movies.reverse()
+movies = movies[::-1]
+with open("movies.txt", "w", encoding="utf8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
 
 
